97d68ef1ce5ae3507cb856b2a3584a5391f8f71d
ParthMandaliya <parth.mandaliya.007@gmail.com>
2022-03-26 12:56:30 +0530

    Shell short implemented completely.

--- Algorithms/ShellSort/shell_sort.py
+++ Algorithms/ShellSort/shell_sort.py
@@ -1,21 +1,28 @@
+def swap(a, b, data):
+    if a != b:
+        data[a], data[b] = data[b], data[a]
+
+
 def shell_sort(data, gap_i=2):
     gap = len(data)//gap_i
     if gap > 0:
-        spaces = [ i for i in range(0, len(data), gap)]
-        #     print (i, "-->", data[i], ",", end=" ")
-        # print ()
-        print (spaces)
+        spaces = [i for i in range(0, len(data), gap)]
+        for i in range(0, len(spaces)):
+            for j in range(0, i):
+                if data[i] < data[j]:
+                    swap(i, j, data)
         gap_i *= 2
         gap = len(data)//(gap_i)
         shell_sort(data, gap_i)
+
 
 if __name__ == '__main__':
     test_cases = [
         [10, 3, 15, 7, 8, 23, 98, 29],
         [],
         [3],
-        [9,8,7,2],
-        [1,2,3,4,5]
+        [9, 8, 7, 2],
+        [1, 2, 3, 4, 5]
     ]
 
     for arr in test_cases:
